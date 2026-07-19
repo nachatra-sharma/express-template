824eb43183813aeff941fa787834e43dd5a22fc9
nachatra-sharma <nakshusharm0@gmail.com>
2026-07-19 15:25:59 +0530

    added: added logger

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,3 +1,15 @@
 import winston from "winston";
 
-const logger = winston.createLogger();
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "DD-MM-YYYY hh:mm:ss" }),
+    winston.format.json(),
+    winston.format.printf(({ timestamp, message, level, ...data }) => {
+      const output = { timestamp, message, level, data };
+      return JSON.stringify(output);
+    }),
+  ),
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import v1Router from "./routers/v1/index.router.js";
 import { ServerConfig } from "./config/index.js";
 import { genericErrorHandler } from "./middlewares/error.middleware.js";
+import logger from "./config/logger.config.js";
 
 const app = express();
 
@@ -12,4 +13,5 @@ app.use(genericErrorHandler);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Server is up and running on port: ${ServerConfig.PORT}`);
+  logger.info("Press ctrl + C to stop the server!!", { data: "Something" });
 });
